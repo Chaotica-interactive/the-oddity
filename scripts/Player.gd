fee0a47d9ab270539c358da647e8182cb5e0d5cc
LuraMoth <fasreaperearnest@gmail.com>
2022-06-23 23:10:58 -0700

    get some very basic movement done

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody
 class_name player
 
 #--------------- VARIABLES ----------------
+export var WALK_SPEED: float = 3.0
 export var MOUSE_SENSITIVITY: float  = 0.2
 
 onready var look_pivot: Spatial = $"look pivot"
@@ -16,13 +17,30 @@ func _ready():
 
 # gets called every time an input is processed
 func _input(event):
+	# do stuff if the mouse is being moved
 	if event is InputEventMouseMotion:
 		# rotate on X
 		rotate_y(deg2rad(-1 * event.relative.x * MOUSE_SENSITIVITY))
-
 		
 		#rotate on Y
 		look_pivot.rotate_x(deg2rad(event.relative.y) * MOUSE_SENSITIVITY)
 
 		# clamp the X rotation
 		look_pivot.rotation.x = clamp(look_pivot.rotation.x, deg2rad(-90), deg2rad(90))
+
+# gets called every physics frame
+func _physics_process(delta):
+	var direction: Vector3 =  get_input_direction()
+
+	move_and_slide(direction * WALK_SPEED, Vector3.UP)
+
+# grabs the current input direction
+func get_input_direction() -> Vector3:
+	var z: float = (
+		Input.get_action_strength("forward") - Input.get_action_strength("backward") #grab the Y movement
+	)
+	var x:float = (
+		Input.get_action_strength("left") - Input.get_action_strength("right") # grab the X movement
+	)
+
+	return Vector3(x, 0, z).normalized()
